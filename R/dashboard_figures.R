135465c2c265491122b7967ade7c0a78eb83050d
Hollister <Hollister.Jeff@epa.gov>
2021-06-30 10:00:14 -0400

    auto download data files

--- R/dashboard_figures.R
+++ R/dashboard_figures.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(lubridate)
 library(ggplot2)
 library(here)
 library(plotly)
@@ -9,7 +10,14 @@ dash_gg <- merged_buoy_data %>%
   filter(name %in% c("primary power", "no3-n conc", "no3-", "temperature",
                      "ph", "odo", "turbidity", "chlorophyll rfu", 
                      "bga-phycocyanin rfu")) %>%
-  ggplot(aes(x=date_time, y = value)) +
+  mutate(year = year(date_time), month = month(date_time), day = day(date_time),
+         hour = hour(date_time), 
+         date_hour = ymd_h(paste(year, month, day, hour))) %>%
+  group_by(waterbody, date_hour, name) %>%
+  filter(value <= quantile(value, c(0.999), na.rm = TRUE)) %>%
+  summarize(value = mean(value, na.rm = TRUE)) %>%
+  ungroup() %>%
+  ggplot(aes(x=date_hour, y = value)) +
   facet_grid(name ~ waterbody, scales = "free") +
   geom_point()
 
