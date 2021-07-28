c8084bd41348806d5bf281ca9195e17e543ddc61
Hollister <Hollister.Jeff@epa.gov>
2021-07-28 11:59:23 -0400

    auto download data files

--- R/dashboard_figures.R
+++ R/dashboard_figures.R
@@ -3,6 +3,8 @@ library(lubridate)
 library(ggplot2)
 library(here)
 library(plotly)
+library(hrbrthemes)
+
 path <- "C:/Users/JHollist/projects/high_res_cyano"
 setwd(path)
 system("git pull")
@@ -12,8 +14,7 @@ load("C:/Users/JHollist/projects/high_res_cyano/data/merged_buoy_data.rda")
 dash_gg <- merged_buoy_data %>%
   filter(name %in% c("no3-", "temperature",
                      "ph", "odo", "chlorophyll rfu", 
-                     "bga-phycocyanin rfu", "rel. barometric pressure",
-                     "wind speed.1", "air temperature")) %>%
+                     "bga-phycocyanin rfu","turbidity")) %>%
   mutate(year = year(date_time), month = month(date_time), day = day(date_time),
          hour = hour(date_time), 
          date_hour = ymd_h(paste(year, month, day, hour))) %>%
@@ -21,10 +22,22 @@ dash_gg <- merged_buoy_data %>%
   filter(value <= quantile(value, c(0.999), na.rm = TRUE)) %>%
   summarize(value = mean(value, na.rm = TRUE)) %>%
   ungroup() %>%
+  mutate(name = factor(name, levels = c("temperature", "ph", "odo", "no3-",
+                                        "chlorophyll rfu", 
+                                        "bga-phycocyanin rfu", "turbidity"),
+                       labels = c("Temp(C)", "pH", "DO(mg/L)", 
+                                  "Nitrate(mg/L)",
+                                  "Chlor.(RFU)", "Phyco(RFU)", 
+                                  "Turbidity(NTU)")),
+         waterbody = factor(waterbody, labels = 
+                              c("Hamblin Pond", "Shubael Pond"))) %>%
   ggplot(aes(x=date_hour, y = value)) +
   facet_grid(name ~ waterbody, scales = "free") +
-  geom_point() + 
-  labs(title = paste("Last Updated:", lubridate::today()))
+  geom_point(aes(color = waterbody)) + 
+  scale_color_manual(values = c("darkred", "darkblue")) +
+  labs(x = "", y = "", title = paste("Last Updated:", lubridate::today())) +
+  theme_bw() +
+  theme(legend.position = "none")
 
 dash_gg_plotly <- ggplotly(dash_gg)
 
