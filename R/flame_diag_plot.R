e4fe44457f154657961281616ffaf081c038cbca
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2022-08-04 16:40:20 -0400

    first cut plot plus some modbus stuff

---
+++ R/flame_diag_plot.R
@@ -0,0 +1,18 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(here)
+
+flame_dat <- read_csv(here("data/FLAMe/satlink/Sutron Satlink 3_log_20220803_20220803_03.csv"),
+                      col_names = FALSE) %>%
+  rename("date" = "X1", 
+         "time" = "X2", 
+         "variable" = "X3", 
+         "value" = "X4", 
+         "units" = "X5", 
+         "qa_flag" = "X6")
+
+flame_dat %>%
+  ggplot(aes(x = time, y = value)) +
+  geom_point() +
+  facet_grid(variable ~ ., scales = "free")
